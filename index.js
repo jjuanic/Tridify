7539ec3ee20e078f5f33e44717ae4b7e96564fae
Juan Chiappella <juanignacioch4@gmail.com>
2023-12-07 09:18:10 -0300

    feature: searchAlbums, searchSongs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 songs = document.getElementById('songs');
 
-const searchMusic = (artist) => {
-    const url = `https://itunes.apple.com/search?term=${artist}&entity=musicTrack`;
+const searchSongs = (artist) => {
+    const url = `https://itunes.apple.com/search?term=${artist}&entity=musicTrack&sort=popular`;
     fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -27,5 +27,31 @@ const searchMusic = (artist) => {
     });
 }
 
+const searchAlbums = (artist) => {
+    const url = `https://itunes.apple.com/search?term=${artist}&entity=album&sort=popular`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const json = data.results;
+        json.forEach(song => {
+        let songDiv = document.createElement('div');
+        let songName = document.createElement('p');
+        songName.innerText = song.collectionName
+        
+        let img = document.createElement('img');
+        img.src = song.artworkUrl100;
+        
+        songDiv.appendChild(songName);
+        songDiv.appendChild(img);
 
-searchMusic('The Beatles');
+        songs.appendChild(songDiv);
+        });
+      console.log(data); 
+    })
+    .catch(error => {
+      console.error('Hubo un problema con la solicitud:', error);
+    });
+}
+
+searchAlbums('Porcupine Tree');
+searchAlbums('The Beatles');
