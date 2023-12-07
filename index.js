69fdd7282b8488db6831feaa076aa0eb056e3896
Juan Chiappella <juanignacioch4@gmail.com>
2023-12-07 10:34:08 -0300

    feature: cards para los álbumes

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 
-songs = document.getElementById('songs');
-
-const searchSongs = (artist) => {
-    const url = `https://itunes.apple.com/search?term=${artist}&entity=musicTrack&sort=popular`;
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const json = data.results;
-        json.forEach(song => {
-        let songDiv = document.createElement('div');
-        let songName = document.createElement('p');
-        songName.innerText = song.trackName;
-        
-        let img = document.createElement('img');
-        img.src = song.artworkUrl100;
-        
-        songDiv.appendChild(songName);
-        songDiv.appendChild(img);
-
-        songs.appendChild(songDiv);
-        });
-      console.log(data); 
-    })
-    .catch(error => {
-      console.error('Hubo un problema con la solicitud:', error);
-    });
-}
+albums = document.getElementById('contenedorAlbumes')
+lista = []
 
 const searchAlbums = (artist) => {
     const url = `https://itunes.apple.com/search?term=${artist}&entity=album&sort=popular`;
@@ -33,20 +8,58 @@ const searchAlbums = (artist) => {
     .then(response => response.json())
     .then(data => {
         const json = data.results;
-        json.forEach(song => {
-        let songDiv = document.createElement('div');
-        let songName = document.createElement('p');
-        songName.innerText = song.collectionName
-        
-        let img = document.createElement('img');
-        img.src = song.artworkUrl100;
-        
-        songDiv.appendChild(songName);
-        songDiv.appendChild(img);
+        json.forEach(album => {
 
-        songs.appendChild(songDiv);
+        // Columna
+        let col = document.createElement('div');
+        col.classList.add('col-md-8', 'mb-4', 'pr-md-4'); 
+
+        //Tarjeta
+        let card = document.createElement('div');
+        card.classList.add("card");
+        card.style.width = "150px"; 
+
+        //Nombre del Albúm
+        let cardTitle = document.createElement('h5');
+        cardTitle.innerText = album.collectionName
+        
+        //Imagén del Albúm
+        let img = document.createElement('img');
+        img.classList.add("card-img-top");
+        img.src = album.artworkUrl100;
+        img.alt = 'album cover';
+
+        // Precio y Género
+        let description = document.createElement('p');
+        description.classList.add("card-text");
+        description.innerText = 'Género: ' + album.primaryGenreName + "\n" + "Precio: " + album.collectionPrice;
+        
+        
+        card.appendChild(cardTitle)
+        card.appendChild(img)
+        card.appendChild(description)
+
+        col.appendChild(card);
+        albums.appendChild(col);
+
+        let botonAgregar = document.createElement('button');
+        botonAgregar.classList.add("btn","btn-success");
+        botonAgregar.innerText = 'Agregar a la Lista';  
+        botonAgregar.addEventListener('click', (e) => {
+            e.preventDefault();
+   
+            albums.push(album);
+   
+            localStorage.setItem('Albums', JSON.stringify(albums));
+        })
+
+
+        card.appendChild(botonAgregar)
+
+        const nombresDeArtistas = json.map((elemento) => elemento.artistName);
+        const resultado = elementoMasRepetido(nombresDeArtistas);
+        console.log(`El elemento más repetido es: ${resultado}`);
         });
-      console.log(data); 
     })
     .catch(error => {
       console.error('Hubo un problema con la solicitud:', error);
@@ -54,4 +67,30 @@ const searchAlbums = (artist) => {
 }
 
 searchAlbums('Porcupine Tree');
-searchAlbums('The Beatles');
+
+function elementoMasRepetido(lista) {
+    let conteo = {};
+  
+    lista.forEach((elemento) => {
+      if (conteo[elemento]) {
+        conteo[elemento]++;
+      } else {
+        conteo[elemento] = 1;
+      }
+    });
+  
+    let elementoMasRepetido;
+    let maxRepeticiones = 0;
+  
+    // Encontrar el elemento con más repeticiones
+    for (let elemento in conteo) {
+      if (conteo[elemento] > maxRepeticiones) {
+        maxRepeticiones = conteo[elemento];
+        elementoMasRepetido = elemento;
+      }
+    }
+  
+    return elementoMasRepetido;
+  }
+  
+
